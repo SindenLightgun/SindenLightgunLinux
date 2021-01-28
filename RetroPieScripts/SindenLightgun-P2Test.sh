5f023f8c671eecf1434114e5a12106473a315e7e
Chris McKeever <cgmckeever@rebelliondefense.com>
2021-01-28 15:44:28 -0600

    change pull repo

--- RetroPieScripts/SindenLightgun-P2Test.sh
+++ RetroPieScripts/SindenLightgun-P2Test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-/home/pi/RetroPie/roms/ports/SindenLightgun-StopAll
-cd /home/pi/SindenLightgun/bin/
+
+/home/pi/RetroPie/roms/ports/SindenLightgun-StopAll.sh
+cd /home/pi/SindenLightgunLinux/bin/
 sudo mono LightgunMono2.exe sdl 30
--- RetroPieScripts/SindenLightgun-StopAll.sh
+++ RetroPieScripts/SindenLightgun-StopAll.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+
 sudo pkill "mono"
 sudo rm /tmp/LightgunMono* -f
