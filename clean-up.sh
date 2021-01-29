7c44b378ab09b356a282a3faa3997ac8d1c7710e
Chris McKeever <cgmckeever@rebelliondefense.com>
2021-01-29 10:48:39 -0600

    attempt per arch setup

--- clean-up.sh
+++ clean-up.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+echo ""
+echo ""
+echo "Cleaning previous Sinden Border Overlays"
 rm -f /opt/retropie/emulators/retroarch/overlays/Sinden*
+
+echo ""
+echo ""
+echo "Cleaning previous Sinden Utilities"
 rm -f /home/pi/RetroPie/roms/ports/*SindenLightgun*
 
 ## bin/executables; but leave configs
 #
+echo ""
+echo ""
+echo "Cleaning previous Architecture Binaries (leaving configs)"
 find bin/ -maxdepth 1 ! -name *exe.config*  -type f -delete
 rm -rf bin/RetroArchPatch
