9231b69c8d3eca8fe8e68945b5c7f878a36c4f32
Chris McKeever <cgmckeever@rebelliondefense.com>
2021-01-29 17:59:45 -0600

    fixing USB script

--- DeviceScripts/p1-add.sh
+++ DeviceScripts/p1-add.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-sleep 10
-#/home/pi/RetroPie/roms/ports/SindenLightgun-P1Start.sh
-cd /home/pi/SindenLightgunLinux/bin/
-sudo mono-service LightgunMono.exe --debug > /tmp/add.txt
+sleep 20
+/home/pi/RetroPie/roms/ports/SindenLightgun-P1Start.sh
