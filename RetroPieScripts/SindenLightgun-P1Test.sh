2a02ca1ff2441cdded96bee27bfa7002f370d53d
Chris McKeever <cgmckeever@rebelliondefense.com>
2021-01-30 09:01:56 -0600

    lockfiles

--- RetroPieScripts/SindenLightgun-P1Test.sh
+++ RetroPieScripts/SindenLightgun-P1Test.sh
@@ -2,5 +2,6 @@
 
 /home/pi/SindenLightgunLinux/DeviceScripts/p1-remove.sh
 
-cd /home/pi/SindenLightgunLinux/bin/
-sudo mono LightgunMono.exe sdl 30
+cd /home/pi/SindenLightgunLinux/bin/; \
+sudo mono LightgunMono.exe sdl 30; \
+cd /home/pi/
--- RetroPieScripts/SindenLightgun-P2Test.sh
+++ RetroPieScripts/SindenLightgun-P2Test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cd /home/pi/SindenLightgunLinux/bin/
-sudo mono LightgunMono2.exe sdl 30
+cd /home/pi/SindenLightgunLinux/bin/; \
+sudo mono LightgunMono2.exe sdl 30; \
+cd /home/pi/
