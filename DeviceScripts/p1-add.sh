a27ebb642a575efb4518f4cdbeacffec8acfad86
Chris McKeever <cgmckeever@rebelliondefense.com>
2021-01-29 18:27:44 -0600

    fixing USB script

--- DeviceScripts/p1-add.sh
+++ DeviceScripts/p1-add.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+end=$((SECONDS+5))
+
+while [ $SECONDS -lt $end ]; do
+    sudo -u pi echo "run" >> /tmp/p1-add.txt
+done
+
 sudo -u pi echo "run" > /tmp/p1-add.txt
 /home/pi/RetroPie/roms/ports/SindenLightgun-P1Start.sh
