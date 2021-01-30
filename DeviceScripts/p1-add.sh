bfdb54373605b0a0a3ff9070b2458b837dd40843
Chris McKeever <cgmckeever@rebelliondefense.com>
2021-01-29 18:12:20 -0600

    fixing USB script

--- DeviceScripts/p1-add.sh
+++ DeviceScripts/p1-add.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 sudo -u pi echo "run" > /tmp/p1-add.txt
-sleep 20
-sudo -u pi /home/pi/RetroPie/roms/ports/SindenLightgun-P1Start.sh
+/home/pi/RetroPie/roms/ports/SindenLightgun-P1Start.sh
--- DeviceScripts/p1-remove.sh
+++ DeviceScripts/p1-remove.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-sudo -u pi echo "run" > /tmp/p1-remove.txt
-sudo -u pi /home/pi/RetroPie/roms/ports/SindenLightgun-StopAll.sh
+/home/pi/RetroPie/roms/ports/SindenLightgun-StopAll.sh
